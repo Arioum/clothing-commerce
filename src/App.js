d54262e026c784f3476a7a2457a8fdf46d4e0a8e
arioum <amoghrao121@gmail.com>
2023-01-12 23:50:13 +0530

    added few base ui components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from "react";
 
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Mens",
+    },
+    {
+      id: 5,
+      title: "Womens",
+    },
+  ];
+
   return (
-    <div className=''>
-      <div className=''></div>
+    <div className='categories-container'>
+      {categories.map(({ title, id }) => (
+        <div key={id} className='category-container'>
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
