9bc1a1ae52b3d1fa0ea068cacf23bcbd0ec8e017
arioum <amoghrao121@gmail.com>
2023-01-21 21:51:09 +0530

    finished google redirect sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,7 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { async } from "@firebase/util";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -6,10 +9,18 @@ const SignIn = () => {
     const userDocRef = await createUserDocumentFromAuth(user);
   }
 
+  useEffect(() => async () => {
+    const response = await getRedirectResult(auth);
+    if(response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  }, [])
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
     </div>
   )
 }
